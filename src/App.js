f765033ea8d651b5e3849fc6d76018d489607114
ZachCodedThat <Zachemailsback@gmail.com>
2022-04-21 14:44:26 -0400

    Initial commit for the Super cool blog project.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Super cool fun time blog app
+      </header>
+    </div>
+  );
+}
+
+export default App;
